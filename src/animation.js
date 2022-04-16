cf62e10270665c048a210322eecc8dbe1b5d0faa
Kirill Strukov <kirillstrukov2@gmail.com>
2022-04-16 14:51:55 +0300

    2/3

--- src/animation.js
+++ src/animation.js
@@ -1,3 +1,20 @@
+// functions
+function offset(el) {
+    const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+// end
+
+// anim-loading
+
+//end
+
+//anim-start
+
+//end
+
 document.addEventListener('mousemove', (e) => {
     gsap.to('.cursor1', {
         left: e.clientX-3,
@@ -44,37 +61,89 @@ gsap.from('.main-offer__h1', {
     stagger: .2,
     ease: "slow(0.7, 0.7, false)",
 })
-const sectionAnimateOffer = document.querySelector('.section-txt-offer');
-sectionAnimateOffer.addEventListener('mouseout', () => {
-    gsap.to('.animation-txt-intro', {
-        left: -20000,
-        duration: 40,
-        ease: 'none',
-    })
-    gsap.to('.animation-txt-intro2', {
-        right: -20000,
-        duration: 40,
-        ease: 'none',
+// const sectionAnimateOffer = document.querySelector('.section-txt-offer');
+// sectionAnimateOffer.addEventListener('mouseout', () => {
+//     gsap.to('.animation-txt-intro', {
+//         left: -20000,
+//         duration: 40,
+//         ease: 'none',
+//     })
+//     gsap.to('.animation-txt-intro2', {
+//         right: -20000,
+//         duration: 40,
+//         ease: 'none',
+//     })
+// })
+// anim-background
+const setionLock = document.querySelector('.section-what-interesting');
+
+let backgroundChange = false;
+
+const bodyForChangeColor = document.querySelector('body');
+const cursorOne = document.querySelector('.cursor1'),
+      cursorTwo = document.querySelector('.cursor');
+window.addEventListener('scroll', () => {
+    if(scrollY > offset(setionLock).top - 300){
+        backgroundChange = true;
+    } 
+    else {backgroundChange = false};
+
+    if(backgroundChange == true){
+        bodyForChangeColor.classList.add('white-style');
+        cursorOne.classList.add('swap-cursor2');
+        cursorTwo.classList.add('swap-cursor1');
+    } else {
+        bodyForChangeColor.classList.remove('white-style');
+        cursorOne.classList.remove('swap-cursor2');
+        cursorTwo.classList.remove('swap-cursor1');
+    }
+
+})
+
+// anim-locomotive
+gsap.registerPlugin(ScrollTrigger);
+const contentTxtOffer = document.querySelectorAll('.content-txt-offer');
+      contentImgOffer = document.querySelectorAll('.content-img');
+
+contentTxtOffer.forEach(el => {
+    console.log(el.children);
+    gsap.to(el, {
+        scrollTrigger: {
+            trigger: el,
+            scrub: 3,
+            start: "20% 90%",
+            end: '30% 50%',
+        },
+        rotate: 0,
+        opacity: 1,
+        y: -100,
+        duration: 4,
+        ease: "slow(0.7, 0.7, false)",
     })
 })
-// let changeColortoScroll = false;
-// window.addEventListener('scroll', ()=> {
-//     const bodyTag = document.querySelector('body');
-//     if(scrollY > 90 && changeColortoScroll == false) {
-//         changeColortoScroll = true;
-//         gsap.to(bodyTag, {
-//             backgroundColor: '#E4F422',
-//             duration: 1,
-//             delay: .5,
-//             ease: "slow(0.7, 0.7, false)",
-//         })
-//     } 
-//     // else (
-//     //     gsap.from(bodyTag, {
-//     //         backgroundColor: '#0C0C0C',
-//     //         duration: 1,
-//     //         delay: .5,
-//     //         ease: "slow(0.7, 0.7, false)",
-//     //     })
-//     // )
-// })
+contentImgOffer.forEach(el => {
+    gsap.to(el, {
+        scrollTrigger: {
+            trigger: el,
+            scrub: 2,
+            start: "bottom bottom",
+        },
+        y: -100,
+        duration: 4,
+        ease: "slow(0.7, 0.7, false)",
+    })
+})
+document.addEventListener('mousemove', (e) => {
+    let onTildWidth = window.innerWidth;
+    let onTildHeight = window.innerHeight;
+    let positionX = (e.clientX/onTildWidth) - 0.55;
+    let positionY = (e.clientY/onTildHeight) - 0.55;
+
+    gsap.to('.content-img', {
+        rotationY: positionX * 30,
+        rotationX: -positionY * 30,
+        ease: 'none',
+
+        })
+})
+//end
